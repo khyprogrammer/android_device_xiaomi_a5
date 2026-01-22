906d67f0f8225df8be663f610b494ddaa48d634d
khyprogrammer <hackerherobrine857@gmail.com>
2026-01-22 14:24:42 +0800

    Update pbrp_a5.mk

--- pbrp_a5.mk
+++ pbrp_a5.mk
@@ -1,21 +1,13 @@
 # Inherit from AOSP 64-bit base (Fixes the "core_64bit.mk" error)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-
-# Inherit PBRP Configuration
 $(call inherit-product, vendor/pb/config/common.mk)
-
-# Inherit device configuration
 $(call inherit-product, device/xiaomi/a5/device.mk)
 
-# Inherit PitchBlack Recovery Project configuration
-$(call inherit-product, vendor/pb/config/common.mk)
-
-# PitchBlack Flags
-PB_RECOVERY_DEVICE := a5
-PB_BUILD_TYPE := Unofficial
-PB_OFFICIAL := false
-PB_DEVICE_MODEL := "Redmi A5"
-PB_MAINTAINER := "khyprogrammer"
+PRODUCT_NAME := pbrp_a5
+PRODUCT_DEVICE := a5
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi A5
+PRODUCT_MANUFACTURER := xiaomi
 
 # A/B and Partition Flags (Still required for Redmi A5)
 PB_VIRTUAL_AB_DEVICE := true
