ba15582ac561c0a68fc264351749ee0ec1331f7f
khy <hackerherobrine857@gmail.com>
2026-01-24 12:34:15 +0800

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo omni_vendor_boot-user
-add_lunch_combo omni_vendor_boot-userdebug
-add_lunch_combo omni_vendor_boot-eng
-
