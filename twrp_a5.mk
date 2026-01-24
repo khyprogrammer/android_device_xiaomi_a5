e70ad31ebeddf80770ac4e25a3b918d41d8559cc
khy <hackerherobrine857@gmail.com>
2026-01-24 12:36:02 +0800

    Create twrp_a5.mk

---
+++ twrp_a5.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/xiaomi/a5/device.mk)
+
+PRODUCT_DEVICE := a5
+PRODUCT_NAME := twrp_a5
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi A5
+PRODUCT_MANUFACTURER := Xiaomi
