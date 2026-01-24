695bbe1de5d6257524b9e5167d6f458b9d1e46dc
khy <hackerherobrine857@gmail.com>
2026-01-24 16:58:27 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_vendor_boot.mk
+    $(LOCAL_DIR)/twrp_a5.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_vendor_boot-user \
-    omni_vendor_boot-userdebug \
-    omni_vendor_boot-eng
+    twrp_a5-user \
+    twrp_a5-userdebug \
+    twrp_a5-eng
 
