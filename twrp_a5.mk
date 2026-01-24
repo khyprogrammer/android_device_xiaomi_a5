8633842212e1210916873055342896d6d3ccf68f
khy <hackerherobrine857@gmail.com>
2026-01-24 13:09:12 +0800

    Add product identifiers for Xiaomi Redmi A5

--- twrp_a5.mk
+++ twrp_a5.mk
@@ -1,6 +1,10 @@
+# Inherit from AOSP base product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from your device configuration
 $(call inherit-product, device/xiaomi/a5/device.mk)
 
+# Product identifiers
 PRODUCT_DEVICE := a5
 PRODUCT_NAME := twrp_a5
 PRODUCT_BRAND := Xiaomi
