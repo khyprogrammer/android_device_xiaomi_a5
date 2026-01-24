36b120f75d7a7865e8ac57e1ffc040fa048051aa
khy <hackerherobrine857@gmail.com>
2026-01-24 16:11:48 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-add_lunch_combo omni_vendor_boot-user
-add_lunch_combo omni_vendor_boot-userdebug
-add_lunch_combo omni_vendor_boot-eng
+add_lunch_combo twrp_a5-user
+add_lunch_combo twrp_a5-userdebug
+add_lunch_combo twrp_a5-eng
+
 
